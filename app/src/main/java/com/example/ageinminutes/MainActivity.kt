4f93b1386fb1d4f28d0042a600ae0282070606ec
kay <kayshiro@live.no>
2021-05-23 16:01:29 +0200

    format Changed

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.ageinminutes
 
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ageinminutes.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
@@ -18,8 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.btnDatePicker.setOnClickListener { view -> //we get the view, then we use the view below
+        binding.btnDatePicker.setOnClickListener { view -> // we get the view, then we use the view below
             clickDatePicket(view)
         }
     }
@@ -29,33 +28,35 @@ class MainActivity : AppCompatActivity() {
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(this,
-                DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
-                    Toast.makeText(this, "The choosen year is $selectedYear, and the month is $selectedMonth, and the day is $selectedDayOfMonth", Toast.LENGTH_LONG).show()
 
-                    //All the calculation belows
-                    val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this, "The choosen year is $selectedYear, and the month is $selectedMonth, and the day is $selectedDayOfMonth", Toast.LENGTH_LONG).show()
 
-                    binding.tvSelectedDate.setText(selectedDate)
+                // All the calculation belows
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
 
-                    //format the date (Simple date format)
-                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.GERMANY)
+                binding.tvSelectedDate.setText(selectedDate)
 
-                    val theDate = sdf.parse(selectedDate) //<-- we converted it to a date object (parse)
+                // format the date (Simple date format)
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.GERMANY)
 
-                    val selectedDateInMinutes = theDate!!.time / 60000
+                val theDate = sdf.parse(selectedDate) // <-- we converted it to a date object (parse)
 
-                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val selectedDateInMinutes = theDate!!.time / 60000
 
-                    val currentDateToMinutes = currentDate!!.time / 60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
 
-                    val differenceInMinutes = currentDateToMinutes - selectedDateInMinutes
+                val currentDateToMinutes = currentDate!!.time / 60000
 
-                    binding.tvSelectedDateInMinute.setText(differenceInMinutes.toString())
+                val differenceInMinutes = currentDateToMinutes - selectedDateInMinutes
 
-                }
-                ,year
-                ,month
-                ,day).show()
+                binding.tvSelectedDateInMinute.setText(differenceInMinutes.toString())
+            },
+            year, month, day
+        )
+        dpd.datePicker.setMaxDate(Date().time - 86400000) // <-- this is just the value of days in millisecond
+        dpd.show()
     }
 }
